36254ce2b1088fe203076f416771cc49c9acd01c
unknown <ntorbev@gmail.com>
2016-10-11 22:35:48 +0100

    second comment

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,34 +1,60 @@
+import java.lang.reflect.Array;
+import java.net.SocketPermission;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 //import org.apache.commons.lang3.ArrayUtils;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Student niki=new Student("niki","tor");
+////        Student[] studentArr =new Student[10];
+//        Random rand=new Random();
+//        Student[] students = new Student[10];
+//        for (int i = 0; i < 10; i++) {
+//            students[i]=new Student("niki"+i, "tor"+i);
+//            students[i].setGrade(rand.nextInt(25-1)+1);
+//        }
+//        Student[] sorted = new Student[10];
+//        int[]exclude =new int[10];
+//        for (int i = 0; i < students.length; i++) {
+//            for (int j = 1; j <students.length-1; j++) {
+////                if (sorted[i]!=null?(!ArrayUtils.contains(exclude, students[j].getGrade()))&&sorted[i].getGrade()>students[j].getGrade()):students[i].getGrade() > students[j].getGrade()){
+//                    sorted[i] = students[j];
+//                    exclude[i]=sorted[i].getGrade();
+////                }
+//            }
+//        }
+//        System.out.println(sorted );
+//
+//        Worker[] wrokers = new Worker[10];
 
-//        niki.setGrade(5);
-//        System.out.println(niki.getGrade());
-//        Student[] studentArr =new Student[10];
-        Random rand=new Random();
         Student[] students = new Student[10];
+        Random rand=new Random();
+
         for (int i = 0; i < 10; i++) {
             students[i]=new Student("niki"+i, "tor"+i);
             students[i].setGrade(rand.nextInt(25-1)+1);
         }
-        Student[] sorted = new Student[10];
-        int[]exclude =new int[10];
-        for (int i = 0; i < students.length; i++) {
-            for (int j = 1; j <students.length-1; j++) {
-//                if (sorted[i]!=null?(!ArrayUtils.contains(exclude, students[j].getGrade()))&&sorted[i].getGrade()>students[j].getGrade()):students[i].getGrade() > students[j].getGrade()){
-                    sorted[i] = students[j];
-                    exclude[i]=sorted[i].getGrade();
-//                }
-            }
-        }
-        System.out.println(sorted );
 
-        Worker[] wrokers = new Worker[10];
+        Arrays.sort(students);
+        System.out.println(students[0].compareTo(students[2]));
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+
+
+        Worker[] workers = new Worker[10];
+        for (int i = 0; i < workers.length; i++) {
+            workers[i]=new Worker("niki"+i,"tor");
+            workers[i].setDaily_wage(i+1);
+            workers[i].setHours_spent_at_work(i+1);
+        }
+        Arrays.sort(workers, Collections.reverseOrder());
+
+        System.out.println();
+
     }
 }
--- OOP/src/Worker.java
+++ OOP/src/Worker.java
@@ -1,30 +1,36 @@
 /**
  * Created by vili on 9/29/2016.
  */
-class Worker extends Human {
-    private int daily_wage;
-    private int hours_spent_at_work;
+class Worker extends Human implements Comparable<Worker>{
+    int daily_wage;
+    int hours_spent_at_work;
 
     Worker(String first_name, String last_name) {
         super(first_name, last_name);
     }
 
-    public void setHours_spent_at_work(int hours_spent_at_work) {
+    void setHours_spent_at_work(int hours_spent_at_work) {
         this.hours_spent_at_work = hours_spent_at_work;
     }
 
-    public int getHours_spent_at_work() {
+    int getHours_spent_at_work() {
         return hours_spent_at_work;
     }
 
-    public int getDaily_wage() {
+    int getDaily_wage() {
         return daily_wage;
     }
-    public void setDaily_wage(int daily_wage) {
+
+    void setDaily_wage(int daily_wage) {
         this.daily_wage = daily_wage;
     }
 
-    public Integer calculate_wage_per_hour(int wage, int hours){
-        return wage/hours;
+    int calculate_wage_per_hour(){
+        return this.daily_wage/this.hours_spent_at_work;
+    }
+
+    @Override
+    public int compareTo(Worker comparedworker) {
+        return comparedworker.calculate_wage_per_hour() > this.calculate_wage_per_hour() ? -1 : 0;
     }
 }
